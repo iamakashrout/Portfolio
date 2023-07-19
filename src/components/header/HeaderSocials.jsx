44ca1c302c56ce942e6207628f1b79fc34221c01
iamakashrout <iamakashrout@gmail.com>
2023-07-19 13:51:23 +0530

    links updated

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -1,22 +1,39 @@
 import React from 'react'
 import { BsLinkedin } from 'react-icons/bs'
 import {BsGithub} from 'react-icons/bs'
-import {BsInstagram} from 'react-icons/bs'
+import { BsInstagram } from 'react-icons/bs'
+import { BsYoutube } from "react-icons/bs";
 
 const HeaderSocials = () => {
   return (
     <div className="header_socials">
       <a
         href="https://www.linkedin.com/in/akash-rout-82a38622a"
+        rel="noreferrer"
         target="_blank"
       >
         <BsLinkedin />
       </a>
-      <a href="https://github.com/iamakashrout" target="_blank">
+      <a
+        href="https://github.com/iamakashrout"
+        rel="noreferrer"
+        target="_blank"
+      >
         <BsGithub />
       </a>
-      <a href="https://www.instagram.com/iamakashrout" target="_blank">
+      <a
+        href="https://www.instagram.com/iamakashrout"
+        rel="noreferrer"
+        target="_blank"
+      >
         <BsInstagram />
+      </a>
+      <a
+        href="https://www.youtube.com/@iamakashrout"
+        rel="noreferrer"
+        target="_blank"
+      >
+        <BsYoutube />
       </a>
     </div>
   );
